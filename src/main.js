839f9db3ca35c52c98bf92a3d97ef7f5109a76ef
Mubashar Ahmad (مبشر احمد) <mubasharahmadqadri@gmail.com>
2023-04-10 16:37:33 +0500

    updated

--- src/main.js
+++ src/main.js
@@ -7,7 +7,8 @@ window.addEventListener('load', function () {
 		type: Phaser.AUTO,
         backgroundColor: "#ffffff",
 		scale: {
-			mode: Phaser.Scale.RESIZE
+			mode: Phaser.Scale.FIT,
+			autoCenter: Phaser.Scale.CENTER_BOTH
 		},
 		fps: {
 			target: 60
